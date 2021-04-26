5d96c0726077fe0d2f65ffe42dc7ffae8d4d94cd
Teemu Kurki <teemu.kurki@gofore.com>
2021-04-26 16:10:12 +0300

    MISPDEV-54 more functions and invoke-rc.d used

--- xtee-misp2-orbeon/install-misp2-orbeon.sh
+++ xtee-misp2-orbeon/install-misp2-orbeon.sh
@@ -8,6 +8,9 @@
 
 xrd_prefix=/usr/xtee
 tomcat_home=/var/lib/tomcat8
+orbeon_deploy_dir=$tomcat_home/webapps/orbeon
+orbeon_config=$orbeon_deploy_dir/WEB-INF/resources/config
+orbeon_config_backup_dir=$(mktemp --directory --tmpdir orbeon_bck.XXXXXX)
 # If set to "true", Orbeon conf files properties-local.xml and log4j.xml are preserved after new webapp deployment
 # This can be used when there are no configuration changes within new Orbeon package.
 preserve_configuration=false
@@ -20,14 +23,11 @@ preserve_configuration=false
 #         failure code (1) if Orbeon deployment directory or conf files do not exist
 ##
 function orbeon_deployed {
-	deploy_dir=$tomcat_home/webapps/orbeon
-	config_dir=$deploy_dir/WEB-INF/resources/config
-
 	# Check whether webapp files exist that indicate deployment in Tomcat
-	if	[[ -d $config_dir 				]]  &&
-		[[ -f $config_dir/properties-local.xml		]]  &&
-		[[ -f $config_dir/theme-orbeon-embedded.xsl	]]  &&
-		[[ -f $config_dir/log4j.xml			]]
+	if	[[ -d $orbeon_config 				]]  &&
+		[[ -f $orbeon_config/properties-local.xml		]]  &&
+		[[ -f $orbeon_config/theme-orbeon-embedded.xsl	]]  &&
+		[[ -f $orbeon_config/log4j.xml			]]
 	then
 		# Webapp has been deployed
 		return 0
@@ -45,7 +45,7 @@ function wait_for_orbeon_deployment {
 	time_spent=""
 	while	! orbeon_deployed
 	do
-		time_spent=$(($SECONDS - $start_time))
+		time_spent=$((SECONDS - start_time))
 		echo -ne "...Waiting for Orbeon webapp deployment... ($time_spent s)"\\r >> /dev/stderr
 		sleep 0.5
 	done
@@ -60,10 +60,9 @@ function wait_for_orbeon_deployment {
 #         failure code (1) if Orbeon deployment directory or WAR exists
 ##
 function orbeon_undeployed {
-	deploy_dir=$tomcat_home/webapps/orbeon
-	war_full_path=$deploy_dir.war
+	war_full_path=$orbeon_deploy_dir.war
 	# Check whether webapp Tomcat deployment directory or the corresponding WAR file exist
-	if	[[ -d $deploy_dir    ]] ||
+	if	[[ -d $orbeon_deploy_dir    ]] ||
 		[[ -f $war_full_path ]]
 	then
 		# WAR or deployment directory still exists, webapp has not yet been undeployed
@@ -82,7 +81,7 @@ function wait_for_orbeon_undeployment {
 	time_spent=""
 	while	! orbeon_undeployed
 	do
-		time_spent=$(($SECONDS - $start_time))
+		time_spent=$((SECONDS - start_time))
 		echo -ne "...Waiting for Orbeon webapp undeployment... ($time_spent s)"\\r >> /dev/stderr
 		sleep 0.5
 	done
@@ -92,20 +91,26 @@ function wait_for_orbeon_undeployment {
 	echo "...Orbeon webapp undeployment done..." >> /dev/stderr
 }
 
+function ensure_tomcat_is_running() {
+	while ! /usr/sbin/invoke-rc.d tomcat8 status > /dev/null 2>&1; do
+        /usr/sbin/invoke-rc.d tomcat8 start > /dev/null
+        sleep 1
+    done
+}
+
+function backup_orbeon_config {
+	cp $orbeon_config/properties-local.xml "$orbeon_config_backup_dir"/
+	cp $orbeon_config/log4j.xml "$orbeon_config_backup_dir"/
+}
+
+function restore_orbeon_config {
+	cp "$orbeon_config_backup_dir"/properties-local.xml $orbeon_config/properties-local.xml
+	cp "$orbeon_config_backup_dir"/log4j.xml $orbeon_config/log4j.xml
+}
 #####################################
 # Begin Orbeon package installation #
 #####################################
 # Check if Tomcat server is running. If it's not, attempt to start.
-status_adverb=
-while ! /etc/init.d/tomcat8 status > /dev/null # do not show output, too verbose
-do
-	echo "Tomcat7 service is not running, attempting to start it."  >> /dev/stderr
-	/etc/init.d/tomcat8 start
-	status_adverb=" now"
-	sleep 1
-done
-echo "Tomcat7 service is$status_adverb running." >> /dev/stderr
-
 
 if [ ! -d $tomcat_home/webapps ]
 then
@@ -113,45 +118,33 @@ then
 	exit 1
 fi
 
+ensure_tomcat_is_running
+
 # Back up configuration, if orbeon has been deployed
 if orbeon_deployed
 then
-	echo " === Backing up configuration === " >> /dev/stderr
-	cp $tomcat_home/webapps/orbeon/WEB-INF/resources/config/properties-local.xml /tmp/properties-local.xml.bkp
-	cp $tomcat_home/webapps/orbeon/WEB-INF/resources/config/log4j.xml /tmp/log4j.xml.bkp
+	backup_orbeon_config
 else
-	# if webapp is not deployed, do not attempt to restore configuration after deploying new webapp
 	preserve_configuration=false
 fi
 
 # Undeploy old war
-echo " === Undeploying previous version of Orbeon web application === " >> /dev/stderr
 rm -rf $tomcat_home/webapps/orbeon*
 wait_for_orbeon_undeployment
 
 # Deploy new orbeon.war to webapps directory
-echo " === Deploying new version of Orbeon web application === " >> /dev/stderr
 cp $xrd_prefix/orbeon/orbeon.war $tomcat_home/webapps/
 wait_for_orbeon_deployment
 
 # Restore version for certain package versions, we dont want to restore for Orbeon version upgrade
 if [ "$preserve_configuration" == "true" ]
 then
-	echo " === Restoring configuration from backup === " >> /dev/stderr
-	cp /tmp/properties-local.xml.bkp $tomcat_home/webapps/orbeon/WEB-INF/resources/config/properties-local.xml
-	cp /tmp/log4j.xml.bkp $tomcat_home/webapps/orbeon/WEB-INF/resources/config/log4j.xml
+	restore_orbeon_config
 fi
 
 # Restart Tomcat
-if [ ! -f /etc/init.d/tomcat8 ]
-then
-        echo "Shutdown Tomcat..." >> /dev/stderr
-        $tomcat_home/bin/shutdown.sh
-        echo "Tomcat starting up..." >> /dev/stderr
-        $tomcat_home/bin/startup.sh
-else
-        /etc/init.d/tomcat8 restart
-fi
+
+/usr/sbin/invoke-rc.d tomcat8 restart       
 
 # Check if Orbeon has been successfully deployed
 if ! orbeon_deployed
