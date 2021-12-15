0200bd40d34f0fc4d1c1f77fee3108b011891d69
Raido Kaju <raido.kaju@gmail.com>
2021-12-15 10:34:51 +0200

    Updated to 2.6.2, bump Log4J2 to 2.16.0

--- xtee-misp2-postgresql/install-misp2-postgresql-debconf.sh
+++ xtee-misp2-postgresql/install-misp2-postgresql-debconf.sh
@@ -24,7 +24,7 @@ pgsql_default_port=5432
 pgsql_default_dbname=misp2db
 pgsql_dir=/usr/lib/postgresql/10/bin
 pgsql_conf_dir=/etc/postgresql/10/main
-current_version="2.6.1"
+current_version="2.6.2"
 
 
 error_prefix="\e[1m\e[91mERROR!\e[0m"
@@ -387,6 +387,10 @@ else
 	if [ "$version" == "2.6.0" ]
     	then
     		version="2.6.1"
+    fi
+	if [ "$version" == "2.6.1" ]
+    	then
+    		version="2.6.2"
     fi
 
 	# Substitute schema name in alter scripts
